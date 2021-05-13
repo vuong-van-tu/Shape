97d9d11ba67b32dc77d30239c204a8f28ccfa5ea
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-13 14:59:40 +0700

    Comparator

--- src/ComparableCircleTest.java
+++ src/ComparableCircleTest.java
@@ -2,17 +2,21 @@ import java.util.Arrays;
 
 public class ComparableCircleTest {
     public static void main(String[] args) {
-        ComparableCircle[] circles = new ComparableCircle[3];
-        circles[0]=new ComparableCircle(3.6);
-        circles[1]=new ComparableCircle();
-        circles[2]=new ComparableCircle(3.5,"indigo",false);
-        System.out.println("Pre-sorted: ");
-        for (ComparableCircle circle : circles){
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[2] = new Circle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
             System.out.println(circle);
         }
-        Arrays.sort(circles);
-        System.out.println("After - sorted :");
-        for (ComparableCircle circle : circles){
+
+        CircleComparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
             System.out.println(circle);
         }
     }
